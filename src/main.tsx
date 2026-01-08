c1e7671b385896108955e91018044aad9facc1bb
oemercem.tabar <oemercem.tabar@studenti.unidp.it>
2026-01-08 15:07:08 +0100

    feat: add react-query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { AppProviders } from './app/providers.tsx'
 import { router } from './app/router.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
   </React.StrictMode>,
 );
